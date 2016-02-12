c03204670384c96e94f8813234766fdcd03b38e7
diego <diego@diegos-mbp.home>
2016-02-12 18:56:05 -0200

    sync

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -52,15 +52,15 @@ return array(
                     ),
                 ),
             ),
-            'agendamento' => array(
-                'type'      => 'Literal',
-                'options'   => array(
-                    'route'     => '/agendamento[/:action[/:id]]',
-                    'defaults'  => array(
-                        'controller' =>
-                    )
-                )
-            )
+//            'agendamento' => array(
+//                'type'      => 'Literal',
+//                'options'   => array(
+//                    'route'     => '/agendamento[/:action[/:id]]',
+//                    'defaults'  => array(
+//                        'controller' =>
+//                    )
+//                )
+//            )
         ),
     ),
     'service_manager' => array(
